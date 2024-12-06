7bdf5161f8be7376cf48b2661fe01aeab48baaf9
Luna <38033777+Hyejeong97@users.noreply.github.com>
2024-12-06 19:14:21 +0900

    Update init.js

--- webapp/js/index/init.js
+++ webapp/js/index/init.js
@@ -50,26 +50,101 @@ $(document).ready(function(){
 			obj.yn = yn;
 			listItems.push(obj);
 		});
-		
-               // const listItems = $('#list li').toArray().map(item => $(item).text());
-    // 배경 이미지 URL (여기서는 예시로 사용, 실제 경로에 맞게 수정)
-    const imgUrl = './webapp/img/form.jpg';
+var pdf = new jsPDF();
+    var img = new Image();
+    img.src = './webapp/img/form.jpg';
 
-    // PDF에 리스트 항목 추가
-    listItems.forEach((item, index) => {
-        const yPosition = 10 + index * 40; // 각 항목의 y 위치 (40씩 증가)
+    function loadImage(src) {
+        return new Promise((resolve, reject) => {
+            var img = new Image();
 
-        // 배경 이미지 추가 (이미지 크기 및 위치 조정)
-        doc.addImage(imgUrl, 'PNG', 5, yPosition - 5, 50, 30);  // (x, y, 너비, 높이)
-
-        // 텍스트 추가 (이미지 위에)
-        doc.text(item.name, 10, yPosition + 5);
-        doc.text(item.amt, 100, yPosition + 13);  // 예시로 x좌표 100에 추가
-      //  doc.text(item.yn, 150, yPosition);  // 예시로 x좌표 150에 추가
+            img.onload = function () {
+                resolve(img);
+            };
+            img.onerror = function () {
+                reject(new Error('Error loading image: ' + src));
+            };
+            img.src = src;
+        });
+    }
     });
 
-    // PDF 파일 다운로드
-    doc.save('가격표.pdf');
+	loadImage(img.src).then(function (loadedImg) {
+        var imgWidth = 60;
+        var imgHeight = 38;
+        var positionX = 15;
+        var positionY = 5;
+        var pageWidth = 210;
+        var pageHeight = 297;
+        var margin = 5;
+        var space = 1;
+        var imagesPerPage = 18; // 한 페이지에 들어갈 이미지 개수
+        var imagesCount = 0; // 페이지에 추가된 이미지 개수
+
+        for (var i = 0; i < listItems.length; i++) {
+            if (imagesCount >= imagesPerPage) {
+                pdf.addPage();
+                imagesCount = 0;
+                positionX = margin + 10;
+                positionY = margin;
+            }
+
+            if (positionX + imgWidth > pageWidth) {
+                positionX = margin + 10;
+                positionY += imgHeight + space;
+            }
+
+            // 이미지를 추가한 후에 이미지 개수를 증가시킴
+            imagesCount++;
+
+            //폰트 추가
+            pdf.addFileToVFS('Cafe24Ohsquare.ttf', _cafe24Ohsquare);
+            pdf.addFont('Cafe24Ohsquare.ttf','Cafe24Ohsquare', 'normal');
+            pdf.setFont('Cafe24Ohsquare');
+
+            // 테두리 추가
+            pdf.setLineWidth(1);
+            pdf.rect(positionX, positionY, imgWidth, imgHeight);
+
+            // 이미지 추가
+            pdf.addImage(loadedImg, 'PNG', positionX, positionY, imgWidth, imgHeight);
+
+            // ITEM_NM에 대한 폰트 및 스타일 설정 (검정색)
+            pdf.setTextColor(0, 0, 0); // 검정색
+            pdf.setFontSize(20);
+
+            var itemTagNm1 = array[i].ITEM_TAG_NM1;
+            var itemTagNm2 = array[i].ITEM_TAG_NM2;
+            if(Array[i].ITEM_TAG_NM2 != ""){
+                pdf.setFontSize(20); // ITEM_NM의 폰트 크기 설정
+                pdf.text(positionX + 6, positionY + 9, itemTagNm1); // ITEM_NM 출력
+                pdf.setFontSize(14); // ITEM_NM2의 폰트 크기 설정
+                pdf.text(positionX + 6, positionY + 15, itemTagNm2); // ITEM_NM2 출력
+            } else {
+                pdf.setFontSize(24); // 기본 폰트 크기로 설정
+                pdf.text(positionX + 4, positionY + 13, itemTagNm1); // ITEM_NM 출력
+            }
+
+
+            // ITEM_PRICE에 대한 폰트 및 스타일 설정 (빨간색)
+            pdf.setTextColor(255, 0, 0); // 빨간색
+            pdf.setFontSize(24);
+            pdf.text(positionX + 10, positionY + 24, array[i].ITEM_PRICE + "원");
+
+            // 바코드 이미지 추가
+            var barcodeDataUrl = createBarcode(array[i].BAR_CODE);
+            var barcodeImg = new Image();
+            barcodeImg.src = barcodeDataUrl;
+            pdf.addImage(barcodeImg, 'PNG', (positionX + imgWidth / 2) - 27, (positionY + imgHeight + 5) -18, imgWidth - 5, (imgHeight / 2) - 5);
+
+            // 원래의 폰트 크기로 되돌리기 (다음 루프에 영향을 주지 않게 하기 위해)
+            pdf.setFontSize(20);
+
+            positionX += imgWidth + space;
+        }
+
+        pdf.save('priceBoard.pdf');
+     
 });
                 
 		
