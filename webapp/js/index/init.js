6cc88624537a544d2586e79b3dd000a3617cf309
Luna <38033777+Hyejeong97@users.noreply.github.com>
2024-12-11 16:44:49 +0900

    Update init.js

--- webapp/js/index/init.js
+++ webapp/js/index/init.js
@@ -98,15 +98,15 @@ $(document).ready(function(){
 
 		// 약간의 대기 후에 로딩바 시작
     		setTimeout(function() {
-	                // jsPDF 객체 생성
-	                const { jsPDF } = window.jspdf;
-	                const doc = new jsPDF();
+			// jsPDF 객체 생성
+			const { jsPDF } = window.jspdf;
+			const doc = new jsPDF();
 	
 			var table = $("#table01");
 			var tbody = table.find("tbody");
 			var tr = tbody.find("tr");
 			
-	                // 리스트 항목 가져오기
+	            	// 리스트 항목 가져오기
 			var listItems = new Array();
 			$.each(tr, function(i, item){
 				var obj = new Object();
@@ -120,7 +120,7 @@ $(document).ready(function(){
 			});
 			var pdf = new jsPDF();
 		    	var img = new Image();
-		   	var imgSrc = './webapp/img/form.jpg';
+			var imgSrc = './webapp/img/form.jpg';
 		
 		        var imgWidth = 60;
 		        var imgHeight = 38;
@@ -136,76 +136,97 @@ $(document).ready(function(){
 		        for (var i = 0; i < listItems.length; i++) {
 				if(listItems[i].yn){
 					imgWidth = 50;
+					imgHeight = 30;
 				}else{
 					imgWidth = 60;
+					imgHeight = 38;
 				}
-		            if (imagesCount >= imagesPerPage) {
-		                pdf.addPage();
-		                imagesCount = 0;
-		                positionX = margin + 10;
-		                positionY = margin;
-		            }
+		            	if (imagesCount >= imagesPerPage) {
+			                pdf.addPage();
+			                imagesCount = 0;
+			                positionX = margin + 10;
+			                positionY = margin;
+		            	}
 		
-		            if (positionX + imgWidth > pageWidth) {
-		                positionX = margin + 10;
-		                positionY += imgHeight + space;
-		            }
+			        if (positionX + imgWidth > pageWidth) {
+					positionX = margin + 10;
+					positionY += imgHeight + space;
+			        }
 		
-		            // 이미지를 추가한 후에 이미지 개수를 증가시킴
-		            imagesCount++;
+			        // 이미지를 추가한 후에 이미지 개수를 증가시킴
+			        imagesCount++;
 		
-		            //폰트 추가
-		            pdf.addFileToVFS('Cafe24Ohsquare.ttf', _cafe24Ohsquare);
-		            pdf.addFont('Cafe24Ohsquare.ttf','Cafe24Ohsquare', 'normal');
-		            pdf.setFont('Cafe24Ohsquare');
+		                //폰트 추가
+		                pdf.addFileToVFS('Cafe24Ohsquare.ttf', _cafe24Ohsquare);
+		                pdf.addFont('Cafe24Ohsquare.ttf','Cafe24Ohsquare', 'normal');
+		                pdf.setFont('Cafe24Ohsquare');
 		
-		            // 테두리 추가
-		            pdf.setLineWidth(1);
-		            pdf.rect(positionX, positionY, imgWidth, imgHeight);
+		               // 테두리 추가
+		              pdf.setLineWidth(1);
+		              pdf.rect(positionX, positionY, imgWidth, imgHeight);
 		
-		            // 이미지 추가
-		            pdf.addImage(imgSrc, 'PNG', positionX, positionY, imgWidth, imgHeight);
+		              // 이미지 추가
+		              pdf.addImage(imgSrc, 'PNG', positionX, positionY, imgWidth, imgHeight);
 		
-		            // ITEM_NM에 대한 폰트 및 스타일 설정 (검정색)
-		            pdf.setTextColor(0, 0, 0); // 검정색
-		            pdf.setFontSize(20);
+		              // ITEM_NM에 대한 폰트 및 스타일 설정 (검정색)
+		              pdf.setTextColor(0, 0, 0); // 검정색
+		              pdf.setFontSize(20);
 		
-		            var itemTagNm1 = listItems[i].name;
-		            var itemTagNm2 = listItems[i].name;
-		             if(listItems[i].amt != ""){
-		                if(itemTagNm1.length > 5){
-					pdf.setFontSize(22); // ITEM_NM의 폰트 크기 설정
-				}else{
-					pdf.setFontSize(30); // ITEM_NM의 폰트 크기 설정
-				}
-		                
-				// 텍스트의 너비 계산
-				var textWidth = pdf.getTextWidth(itemTagNm1);
-				// 이미지의 중앙 위치를 기준으로 텍스트의 위치 조정
-				var centeredX = positionX + (imgWidth - textWidth) / 2;
-				pdf.text(centeredX, positionY + 13, itemTagNm1);
-		            } else {
-		                pdf.setFontSize(24); // 기본 폰트 크기로 설정
-		                pdf.text(positionX + 4, positionY + 13, itemTagNm1); // ITEM_NM 출력
-		            }
+		              var itemTagNm1 = listItems[i].name;
+		              var nmYMargin = 13;
+		            
+		        	nmYMargin = nmYMargin != 13 ? 13 : nmYMargin;
+		            if(itemTagNm1.length > 5){
+						if(!listItems[i].yn){
+							pdf.setFontSize(22); // ITEM_NM의 폰트 크기 설정
+						}else{
+							nmYMargin -= 3;
+							pdf.setFontSize(18); 
+						}
+					}else{
+						if(!listItems[i].yn){
+							pdf.setFontSize(30); // ITEM_NM의 폰트 크기 설정
+						}else{
+							nmYMargin -= 3;
+							pdf.setFontSize(26); 
+						}
+					}
+					// 텍스트의 너비 계산
+					var textWidth = pdf.getTextWidth(itemTagNm1);
+					// 이미지의 중앙 위치를 기준으로 텍스트의 위치 조정
+					var centeredX = positionX + (imgWidth - textWidth) / 2;
+					pdf.text(centeredX, positionY + nmYMargin, itemTagNm1);
+						 
+		            
 					
-		            // ITEM_PRICE에 대한 폰트 및 스타일 설정 (빨간색)
-		            pdf.setTextColor(255, 0, 0); // 빨간색
-		            //pdf.setFontSize(50);
-			    if(listItems[i].amt.length > 4){
-				pdf.setFontSize(45); // ITEM_NM의 폰트 크기 설정
-			    }else{
-				    space = space != 1 ? 1 : space;
-				if(!listItems[i].yn){
-					positionX += 3;
-					
-				}else{
-					positionX -= 2;
-					space += 2;
-				}
-				pdf.setFontSize(48); // ITEM_NM의 폰트 크기 설정
-			    }
-		            pdf.text(positionX + 5, positionY + 30, listItems[i].amt);
+					// 금액 설정
+					pdf.setTextColor(255, 0, 0); // 빨간색
+					var amtYMargin = 30;
+					amtYMargin = amtYMargin != 30 ? 30 : amtYMargin;
+				    if(listItems[i].amt.length > 4){
+						if(!listItems[i].yn){
+							positionX += 3;
+							pdf.setFontSize(38); // ITEM_NM의 폰트 크기 설정
+						}else{
+							positionX += 3;
+							amtYMargin -= 6;
+							pdf.setFontSize(32); 
+						}
+						
+				    }else{
+					    space = space != 1 ? 1 : space;
+						if(!listItems[i].yn){
+							positionX += 3;
+							pdf.setFontSize(48); // ITEM_NM의 폰트 크기 설정
+						}else{
+							amtYMargin -= 5;
+							//positionX -= 2;
+							space += 2;
+							pdf.setFontSize(41); // ITEM_NM의 폰트 크기 설정
+						}
+						
+				    }
+		            pdf.text(positionX + 5, positionY + amtYMargin, listItems[i].amt);
 		
 		
 		            // 원래의 폰트 크기로 되돌리기 (다음 루프에 영향을 주지 않게 하기 위해)
@@ -213,31 +234,30 @@ $(document).ready(function(){
 		
 		            positionX += imgWidth + space;
 	
-			    // 로딩바 진행 업데이트
-			    var progress = Math.floor(((i + 1) / listItems.length) * 100); // 진행률 계산
-			    $('#loading-bar').width(progress + '%'); // 로딩바 업데이트
-			    $('#loading-percent').text(progress + '%'); // 퍼센트 업데이트
+				    // 로딩바 진행 업데이트
+				    var progress = Math.floor(((i + 1) / listItems.length) * 100); // 진행률 계산
+				    $('#loading-bar').width(progress + '%'); // 로딩바 업데이트
+				    $('#loading-percent').text(progress + '%'); // 퍼센트 업데이트
 		        }
-			var today = new Date();
-		
-			var year = today.getFullYear();
-			var month = ('0' + (today.getMonth() + 1)).slice(-2);
-			var day = ('0' + today.getDate()).slice(-2);
-			
-			var dateString = year + month  + day;
+				var today = new Date();
+				var year = today.getFullYear();
+				var month = ('0' + (today.getMonth() + 1)).slice(-2);
+				var day = ('0' + today.getDate()).slice(-2);
+				
+				var dateString = year + month  + day;
 	
-			// PDF 저장 전에 로딩바 업데이트
+				// PDF 저장 전에 로딩바 업데이트
 	    		$('#loading-bar').width('100%'); // 로딩 완료 상태로 설정
-			$('#loading-percent').text('100%'); // 퍼센트 완료
+				$('#loading-percent').text('100%'); // 퍼센트 완료
 		
 		        pdf.save('착한가게24_가격표_' + dateString + '.pdf');
 	
-			// 다운로드 완료 후 로딩바 숨김
-			setTimeout(function() {
-				$('#prtOverlay').hide();
-				$('#loading-container').hide();
-				$('#loading-percent').hide();
-			}, 500);
+				// 다운로드 완료 후 로딩바 숨김
+				setTimeout(function() {
+					$('#prtOverlay').hide();
+					$('#loading-container').hide();
+					$('#loading-percent').hide();
+				}, 500);
      		}, 100); // 약간의 대기 후 PDF 작업 시작
 
 	});
